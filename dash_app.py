b93a2db7e9c2e46dacc09888f62e0bdbe6115c96
YeOldProgrammer <github@olker.us>
2020-12-30 22:32:46 -0600

    Dynamic Update Update

--- dash_app.py
+++ dash_app.py
@@ -1,9 +1,11 @@
 import logging
+import json
 import dash
 import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import dash_html_components as html
 from app_code import rpg_data as rd, parse_life_paths as lp
+from app_code import gen_character as gc
 
 LOGGER = logging.getLogger('gui')
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -13,28 +15,126 @@ skill_options = [{'label': 'None', 'value': 'None'}]
 for skill in sorted(rd.CHAR_DATA.skill_dict):
     skill_options.append({'label': skill, 'value': skill})
 
-character_start_div = html.Div([
-    dbc.Modal([
+div_character_start = html.Div(
+    id='div_character_start',
+    style=dict(display="none"),
+    children=[
+        html.H3("Enter Starting Data"),
+        html.Button("Submit", id='btn_accept_start', style=dict(marginLeft=10)),
+    ],
+)
 
-    ])
-])
+
+div_select_life_path = html.Div(
+    id='div_select_life_path',
+    style=dict(display='none'),
+    children=[
+        html.Div([
+            html.Label("Lifepath skill filter:  ",
+                       style=dict(verticalAlign='middle', display='inline-block', fontWeight='bold')),
+            dcc.Checklist(id='options', options=[{'label': 'Childhood', 'value': 'childhood'}], value=['childhood'],
+                          style=dict(verticalAlign='middle', display='inline-block', marginLeft="10px")),
+            dcc.Dropdown(id='skills', options=skill_options, multi=True,
+                         style=dict(display='inline-block', width="70%", verticalAlign="middle", marginLeft="10px")),
+        ]),
+        html.P(id='results'),
+    ]
+)
+
+
+div_confirm_life_path = html.Div(
+    id='div_confirm_life_path',
+    style=dict(display='none'),
+    children=[
+        html.H3("Select Life Path"),
+        html.Button("Accept", id='btn_submit_life_path', style=dict(marginLeft=10)),
+   ]
+)
+
+
+div_confirm_general_skills = html.Div(
+    id='div_confirm_general_skills',
+    style=dict(display='none'),
+    children=[
+        html.H3("Select Life Path"),
+        html.Button("Accept", id='btn_submit_general_skills', style=dict(marginLeft=10)),
+   ]
+)
+
+
+div_confirm_results = html.Div(
+    id='div_confirm_results',
+    style=dict(display='none'),
+    children=[
+        html.H3("Select Life Path"),
+        html.Button("Accept", id='btn_accept_life_path', style=dict(marginLeft=10)),
+   ]
+)
 
 
 app.layout = html.Div([
-    html.H1("Search for the following skills in the same lifepath."),
-    # html.Button("Submit", id='submit', n_clicks=0, style=dict(display="inline-block")),
     html.Div(children=[
-        html.Label("Lifepath skill filter:  ",
-                   style=dict(verticalAlign='middle', display='inline-block', fontWeight='bold')),
-        dcc.Checklist(id='options', options=[{'label': 'Childhood', 'value': 'childhood'}], value=['childhood'],
-                      style=dict(verticalAlign='middle', display='inline-block', marginLeft="10px")),
-        dcc.Dropdown(id='skills', options=skill_options, multi=True,
-                     style=dict(display='inline-block', width="70%", verticalAlign="middle", marginLeft="10px")),
+        html.H1("Character Generation"),
+        html.Button("Back", id='btn_back', style=dict(display='none', marginLeft=10)),
+        html.Button("Clear", id='btn_clear', style=dict(display='inline-block', marginLeft=10)),
+        html.Button("Load", id='btn_load', style=dict(display='inline-block', marginLeft=10)),
+        html.Hr(),
     ], style=dict(float='left', width="100%", display="inline-block")),
-    html.Br(),
-    html.Br(),
-    html.Div(id='results')
+
+    div_character_start,
+    div_select_life_path,
+    div_confirm_life_path,
+    div_confirm_general_skills,
+    div_confirm_results,
+    html.Div(id='character_data', style={'display': 'none'})
 ])
+
+
+@app.callback(
+    [
+        dash.dependencies.Output('div_character_start', 'style'),
+        dash.dependencies.Output('div_select_life_path', 'style'),
+        dash.dependencies.Output('div_confirm_life_path', 'style'),
+        dash.dependencies.Output('div_confirm_general_skills', 'style'),
+        dash.dependencies.Output('div_confirm_results', 'style'),
+    ],
+    [
+        dash.dependencies.Input('btn_back', 'n_clicks'),
+        dash.dependencies.Input('btn_clear', 'n_clicks'),
+        dash.dependencies.Input('btn_load', 'n_clicks'),
+        dash.dependencies.Input('btn_accept_start', 'n_clicks'),
+        dash.dependencies.Input('btn_submit_life_path', 'n_clicks'),
+        dash.dependencies.Input('btn_submit_general_skills', 'value'),
+        dash.dependencies.Input('btn_accept_life_path', 'n_clicks'),
+    ],
+    [
+        dash.dependencies.State('character_data', 'children'),
+    ]
+)
+def master_callback(back_clicks, clear_clicks, load_clicks, accept_start_clicks, submit_life_path_clicks,
+                    submit_general_skills_clicks, accept_life_path_clicks, character_data):
+
+    if character_data is not None:
+        try:
+            json.loads(character_data)
+        except Exception as error_text:
+            LOGGER.error("Failed to load character data - %s", str(error_text))
+            character_data = gc.BLANK_CHARACTER
+    else:
+        character_data = gc.BLANK_CHARACTER
+
+    LOGGER.info("back:%s clear:%s load:%s accept_start:%s submit_life_path:%s submit_general_skills:%s accept_life_path:%s\n%s",
+                back_clicks, clear_clicks, load_clicks, accept_start_clicks, submit_life_path_clicks,
+                submit_general_skills_clicks, accept_life_path_clicks, json.dumps(character_data, indent=4)
+                )
+
+    display_none = dict(display='none')
+    display_show = dict(display='inline')
+
+    if character_data['potential_used'] == 0:
+        return display_show, display_none, display_none, display_none, display_none
+
+    return display_none, display_none, display_none, display_none, display_none
 
 
 @app.callback(
@@ -43,9 +143,8 @@ app.layout = html.Div([
         dash.dependencies.Input('skills', 'value'),
         dash.dependencies.Input('options', 'value'),
     ],
-
 )
-def search_lifepaths(skill_list, options):
+def refresh_lifepaths(skill_list, options):
     output = []
     if skill_list is not None:
         skill_count = len(skill_list)
